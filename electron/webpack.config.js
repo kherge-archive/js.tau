78ddd848b2d0e0ab4eed7aefd4a0c3f482373f49
Kevin Herrera <kevin@herrera.io>
2021-04-05 18:56:51 -0700

    Generating main script for Electron.
    The react-scripts configuration is re-used in order to build a self contained version of setupElectron.ts.

---
+++ electron/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = "production";
+}
+
+// Re-use the react-scripts Webpack configuration.
+const config = require("react-scripts/config/webpack.config.js")();
+
+// Target Node.js.
+config.target = "electron-main";
+
+// Remove the HTML plugins.
+config.plugins = config.plugins.filter((plugin) => {
+  const name = plugin.constructor.name;
+
+  switch (name) {
+    case "HtmlWebpackPlugin":
+    case "InterpolateHtmlPlugin":
+    case "ManifestPlugin":
+      return false;
+  }
+
+  return true;
+});
+
+// Change the entrypoint to the Electron script.
+config.entry = path.join(__dirname, "../src/setupElectron.ts");
+
+// Output to a single file.
+config.output = {
+  filename: "setup.js",
+  path: path.join(__dirname, "../build"),
+};
+
+// Disable chunking.
+config.optimization.splitChunks = false;
+config.optimization.runtimeChunk = false;
+
+module.exports = config;
